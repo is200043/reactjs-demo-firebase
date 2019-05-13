66f71781949e7de909e27aea9a63604bb9155e2a
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2019-05-13 20:47:01 +0700

    first commit

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions');
+const modemailer = require('nodemailer');
+const emailAccount = require('./email.json');
+
+const mailTransport = modemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: emailAccount.email,
+        pass: emailAccount.password
+    }
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase! hahah");
+});
+
+exports.moltinPayment = functions.https.onRequest((request, response) => {
+    const email = 'boatclubonline@gmail.com';
+    const orderId = 'abcdf';
+    const mailOptions = {
+        to: email,
+        bcc: 'chonlakorn.pun@gmail.com',
+        form: '',
+        subject: `Recieve Order No. ${orderId}`,
+        // html: ejs.render(orderCustomerConfirm, {value}),
+        text: `Dekhippozaa store recieve your order ${orderId} now.\n Thank for shop`
+    }
+    mailTransport.sendMail(mailOptions);
+    response.send({ success: true });
+});
+
